5892a1d6f8b948b32b9252d0a1ce64789bee27e1
Sukkiii <knk951213@naver.com>
2023-01-08 18:12:39 +0900

    feat: 네이버 로그인 추가

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="login-msg">
+      네이버를 더 안전하고 편리하게 이용하세요
+    </div>
+    <button class="naver-login">
+      NAVER 로그인
+    </button>
+    <div class="sub-area">
+      <div class="left">
+        <button class="button">
+          아이디
+        </button>
+        <button class="button">
+          비밀번호
+        </button>
+      </div>
+      <button class="button">
+        회원가입
+      </button>
+    </div>
+    
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss" scoped>
+.container {
+  background-color: #f7f9fa;
+  border: 1px solid #dae1e6;
+  padding: 1.2rem;
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.login-msg{
+  color: gray;
+  padding-left: 3px;
+  text-align: left;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.naver-login{
+  padding: 1.5rem 0;
+  width: 100%;
+  margin-top: 11px;
+  background-color: #19ce60;
+  border:1px solid #15c654;
+  font-size:13px;
+  color: #fff;
+  font-weight: 700;
+  text-align: center;
 }
-a {
-  color: #42b983;
+.sub-area{
+  height: 3rem;
+  display: flex;
+  justify-content: space-between;
+  .left {
+    display: flex;
+  }
+  .button {
+    color:#404040;
+    letter-spacing: -.2px;
+    border: none;
+    background-color: transparent;
+  }
 }
+
 </style>
